f9d4bd4f5d4af07776c0c45e588f299cdb0f300c
cinaral <cinar.al@outlook.com>
2023-03-31 00:05:16 -0500

    Update license text

--- include/rt_timer.hpp
+++ include/rt_timer.hpp
@@ -3,7 +3,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2022 Cinar, A. L.
+ * Copyright (c) 2023 Cinar, A. L.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -24,8 +24,8 @@
  * SOFTWARE.
  */
 
-#ifndef rt_timer_HPP_CINARAL_220923_1707
-#define rt_timer_HPP_CINARAL_220923_1707
+#ifndef rt_timer_HPP_CINARAL_230331_0004
+#define rt_timer_HPP_CINARAL_230331_0004
 
 #include "rt_timer/timer.hpp"
 #include "rt_timer/timer_thread.hpp"
